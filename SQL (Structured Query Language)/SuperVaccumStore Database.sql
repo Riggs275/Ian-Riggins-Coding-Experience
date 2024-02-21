81065d03e0ee5f1e331691e68452b25a601c0d4a
Ian Riggins <155788213+Riggs275@users.noreply.github.com>
2024-02-20 20:13:37 -0500

    SQL Projects
    Added SQL projects to the projects folder along with the corresponding ER diagrams

---
+++ SQL (Structured Query Language)/SuperVaccumStore Database.sql
@@ -0,0 +1,238 @@
+﻿CREATE DATABASE SuperVaccumStore;
+/* Creates the Database */
+
+/* This section contains the code where Entity tables are created */
+
+CREATE TABLE Vacuum (
+SerialNumber INT PRIMARY KEY,
+Manufacture_CompletionDate DATE);
+/* Creates Table for Vacuum Entity using the Serial Number as a Primary Key */
+
+CREATE TABLE ModelDetails (
+ID_Number INT PRIMARY KEY,
+Model_Name VARCHAR(50),
+Color VARCHAR(10),
+Price DECIMAL(15, 2));
+/* Creates Table for Model (Vacuum Model Details) Entity using Identification Number as a Primary Key */
+
+CREATE TABLE Designer (
+DesignerID INT PRIMARY KEY,
+FirstName VARCHAR(20),
+MiddleInitial VARCHAR(2),
+LastName VARCHAR(30),
+Email VARCHAR(50),
+PhoneNumber VARCHAR(18),
+StreetName VARCHAR(50),
+ZipCode INT,
+City VARCHAR(20),
+Country VARCHAR(20),
+DateofBirth DATE,
+AGE INT);
+/* Creates Table for the Designer (Vacuum Designer) Entity using Designer ID as a Primary Key */
+
+CREATE TABLE Technicians (
+EmployeeNumber INT PRIMARY KEY,
+HireDate DATE,
+YearswithCompany INT,
+FirstName VARCHAR(20),
+MiddleInitial VARCHAR(2),
+LastName VARCHAR(30),
+Email VARCHAR(50),
+PhoneNumber VARCHAR(18),
+StreetName VARCHAR(50),
+ZipCode INT,
+City VARCHAR(20),
+Country VARCHAR(20),
+DateofBirth DATE,
+Age INT);
+/* Creates Table for the Techician (Vacuum Technician) Entity using Employee Number as a Primary Key */
+
+/* This indicates the end of the section for creating Entity Tables */
+
+
+/* This section contains the code for creating Associative Entity Tables */
+
+CREATE TABLE ModelDesigner (
+Identification_Number INT FOREIGN KEY REFERENCES ModelDetails(ID_Number),
+Designer_ID INT FOREIGN KEY REFERENCES Designer(DesignerID),
+PRIMARY KEY (Designer_ID, Identification_Number));
+/* Creates Table for the Model-Designer (Designs) Associative Entity using Identification Number & Designer ID as a composite key*/
+
+CREATE TABLE VacuumTechnician (
+Serial_Number INT FOREIGN KEY REFERENCES Vacuum(SerialNumber),
+Employee_Number INT FOREIGN KEY REFERENCES Technicians(EmployeeNumber),
+VerificationDate DATE,
+VacuumQuality VARCHAR(20)
+PRIMARY KEY (Serial_Number, Employee_Number));
+/* Creates Table for the Vacuum-Technicians (Inspection) Associative Entity using Serial Number & Employee Number as a composite key */
+
+/* This indicates the end of the section for creating Associative Entity Tables */
+
+
+/* This section contains code for inserting data into tables */ 
+
+INSERT INTO Vacuum 
+VALUES 
+(012, '2012-06-23'), 
+(090, '2010-11-11'),
+(031, '2016-10-25'),
+(051, '2011-07-14'), 
+(039, '2007-10-04'),
+(079, '2012-11-18'),
+(086, '2012-11-18'),
+(025, '2009-11-12'),
+(037, '2008-04-10'),
+(066, '2010-05-23');
+/* Insert 10 rows of data into Vacuum table */
+
+INSERT INTO ModelDetails
+VALUES
+(039, 'Hydro Vacuum 5000', 'Turquoise', 1280.74),
+(079, 'Basic Vacuum', 'Gray', 226.74),
+(086, 'Tight Spot Vacuum', 'Yellow', 2357.20),
+(025, 'Pokemon Special Event Vacuum', 'Teal', 3292.55),
+(037, 'Deep Clean Vacuum', 'Violet', 1860.44),
+(066, 'Dirt Dectection Vacuum', 'Gold', 1321.37),
+(090, 'Super Mario Promotion Vacuum', 'Scarlet', 3488.90),
+(012, 'Nature Friendly Vacuum', 'Green', 1556.78),
+(031, 'Quick Pickup Vacuum', 'Brown', 2805.75),
+(051, 'Infinite Void Vacuum', 'Black', 2158.62);
+/* Inserts 10 rows of data into Model Details Table */
+
+INSERT INTO Designer
+VALUES
+(6533, 'Naruto', 'W.', 'Uzumaki', '7thHokage@HotLeaf.com', '(837) 345-0715', 'Sydney Roads', 3102, 'New Daijaport', 'United States', '1985-10-10', NULL),
+(4965, 'Sasuke', 'P.', 'Uchiha', 'ShadowHokage@HotLeaf.com', '(247) 904-9933', 'Hector Road', 891, 'Alexaview', 'United States', '1985-07-23', NULL),
+(8183, 'Rock', 'L.', 'Lee', 'BurningYouthShinobimail.org', '(484) 933-6546', 'Collins Causeway', 17713, 'Alisabury', 'United States', '1984-11-27', NULL),
+(1563, 'Kakashi', 'W.', 'Hatake', '6thHokahe@HotLeaf.com', '(782) 635-3665', 'Xavier Junction', 7988, 'Bodeberg', 'Australia', '1971-09-15', NULL),
+(8065, 'Guy', 'O.', 'Might', 'PowerofYouth@HotLeaf.com', '(342)-248-5794', 'Feil Circuit', 5896, 'Mcdermottview', 'Australia', '1971-01-01', NULL),
+(5533, 'Itachi', 'K.', 'Uchiha', 'MangekyoSharingan@Shinobimail.org', '(985) 500-6558', 'Schamberger Brooks', 85193, 'Tysonborough', 'Canada', '1980-06-09', NULL),
+(9522, 'Tsunade', 'U.', 'Senju', 'SlugSage@Yohaa.org', '(770) 860-0651', 'Victor Landing', 2977, 'Durban', 'South Africa', '1947-08-02', NULL),
+(7731, 'Obito', 'G.', 'Uchiha', 'ProjectTsukenome@Shinobimail.org', '(890) 649-0435', 'Moore Tunnel', 728, 'West Ryanhaven', 'Great Britain', '1970-02-10', NULL),
+(8494, 'Minato', 'N.', 'Namikaze', 'YellowFlash@Yohaa.org', '(647) 865-3461', 'Skyla Gardens', 164, 'South Carloshaven', 'United States', '1958-01-25', NULL),
+(3951, 'Hinata', 'H.', 'Hyūga', 'TwinLions@HotLeaf.com', '(671) 215-7236', 'Sydney Roads', 3102, 'New Daijaport', 'United States', '1986-12-27', NULL);
+/* Inserts 10 rows of data into Designer Table */
+
+INSERT INTO Technicians
+VALUES
+(05, '2009-12-22', NULL, 'Luffy', 'D.', 'Monkey', 'FuturePirateKing@email.com', '(249) 537-4804', 'Melany Square', 69521, 'Schadenshire', 'United States', '1983-05-05', NULL),
+(01, '1989-12-10', NULL, 'Doflamingo', 'C.', 'Donquixote', 'PuppetMaster@email.com',  '(848) 986-3045', 'Auer Courts', 41570, 'Port Destineemouth', 'United States', '1961-10-23', NULL),
+(08, '2002-02-05', NULL, 'Zoro', 'F.', 'Roronoa', 'Santoryu@email.com', '(957) 644-6021', 'Darby Knoll', 0937, 'Bentonburgh', 'United States', '1981-11-11', NULL),
+(06, '2003-11-15', NULL,  'Chopper', 'N.', 'Tony Tony', 'ReindeerDoctor@email.com', '(593) 882-7771', 'Kimberly Road', 960, 'West Luz', 'United States', '1985-12-24', NULL),
+(03, '2002-05-02', NULL, 'Sanji', 'J.', 'Vinsmoke', 'BlackLegChef@email.com', '(654) 136-0087', 'Green Views', 05317, 'East Jairofort', 'United States', '1981-03-08', NULL),
+(02, '2002-08-10', NULL, 'Robin', 'A.', 'Nico', 'PoneglyphHunter@email.com', '(991) 868-4560', 'Langworth Roads', 288, 'East Hettie', 'United States', '1970-02-06', NULL),
+(07, '1999-09-19', NULL, 'Law', 'D.', 'Trafalgar Water', 'RoomShambles@email.com', '(894) 942-0985', 'Vincenta Summit', 58480, 'Gabriella', 'United States', '1976-10-06', NULL),
+(04, '1998-04-18', NULL, 'Ace', 'D.', 'Portgas', 'Hiken@email.com', '(608) 998-1125', 'Water Circles', 5679, 'Cristinashire', 'United States', '1979-01-01', NULL),
+(10, '1968-04-19', NULL, 'Roger', 'D.', 'Gol', 'CurrentPirateKing@email.com', '(505) 534-9662', 'Mikel Well', 89311, 'New Darryl', 'Ireland', '1929-12-31', NULL),
+(09, '1969-02-24', NULL, 'Rayleigh', 'J.', 'Silvers', 'DarkKing@email.com', '(336) 851-2654', 'Wilma Fords', 03179, 'Lake Jerell', 'Ireland', '1924-05-13', NULL);
+/* Inserts 10 rows of data into Technicians Table */
+
+INSERT INTO ModelDesigner
+VALUES
+(090, 8183),
+(037, 8183),
+(039, 8065),
+(051, 7731),
+(086, 1563),
+(025, 5533),
+(031, 8494),
+(012, 6533),
+(066, 4965), 
+(037, 3951);
+/* Inserts 10 rows of data into Model-Designer Table */
+
+INSERT INTO VacuumTechnician
+VALUES
+(051, 02, '2022-03-15', 'Good'),
+(025, 01, '2022-03-25', 'Bad'),
+(079, 03, '2022-04-01', 'Average'),
+(066, 04, '2022-04-05', 'Good'),
+(090, 08, '2022-10-11', 'Good'),
+(039, 08, '2022-04-12', 'Average'),
+(086, 09, '2022-06-30', 'Bad'),
+(037, 06, '2022-09-25', 'Good'),
+(012, 05, '2022-01-23', 'Average'),
+(031, 10, '2022-10-20', 'Good');
+/* Inserts 10 rows of data into Vacuum-Technician Table */
+
+/* This indicates the end of the section for inserting data into tables */
+
+
+/* This section contains codes for updating derived attributes (It wouldn't let me do it while inserting) */
+
+UPDATE Technicians
+SET YearswithCompany = DATEDIFF(yy,HireDate,'2022-11-12');
+
+UPDATE Technicians
+SET AGE = DATEDIFF(yy, DateofBirth,'2022-11-12');
+
+UPDATE Designer
+SET AGE = DATEDIFF(yy, DateofBirth,'2022-11-12');
+
+/* This indicates the end of the section for updating derived attributes */
+
+
+/* This section contins code for retrieving all the original tables per submission rule 1 */
+
+SELECT *
+FROM Vacuum;
+/* Retrieves all data from original Vacuum table */
+
+SELECT *
+FROM ModelDetails;
+/* Retrieves all data from original ModelDetails table */
+
+SELECT *
+FROM Designer;
+/* Retrieves all data from original Designer table */
+
+SELECT *
+FROM Technicians;
+/* Retrieves all data from original Technicians table */
+
+SELECT *
+FROM ModelDesigner;
+/* Retrieves all data from original Model-Designer table */
+
+SELECT *
+FROM VacuumTechnician;
+/* Retrieves all data from original Vacuum-Technician table */
+
+/* This indicates the end of the section for retrieving all the original tables */
+
+
+/* This section contains code for retrieving all tables based off the questions per submission rule 2 */
+
+SELECT Vacuum.SerialNumber AS VacuumID, ModelDetails.Model_Name AS Name_of_Model, ModelDetails.Color AS VacuumColor, ModelDetails.Price AS Cost
+FROM Vacuum
+JOIN ModelDetails ON Vacuum.SerialNumber = ModelDetails.ID_Number;
+/* Retrieves the model of each vacuum and its price */
+
+SELECT *
+FROM Vacuum
+WHERE Manufacture_CompletionDate > '2010-12-31';
+/* Retrieves all vacuums that were manufactured after 2010 */
+
+SELECT LastName + ' ' + MiddleInitial + ' ' + FirstName AS TechnicianName, EmployeeNumber
+FROM Technicians
+WHERE EmployeeNumber = 8;
+/* The name of the technician who has inspected the most number of vacuums */
+
+SELECT ModelDetails.ID_Number AS SerialNumber, ModelDetails.Model_Name AS VacuumModel, VacuumTechnician.VacuumQuality AS QualityGrade, VacuumTechnician.VerificationDate AS Date_Verified
+FROM VacuumTechnician
+JOIN ModelDetails ON VacuumTechnician.Serial_Number = ModelDetails.ID_Number
+WHERE VacuumQuality = 'Average';
+/* Retrieves all vacuum models that received an average grading and their verification date */
+
+SELECT Vacuum.SerialNumber AS Vacuum_SerialNumber, Designer.FirstName + ' ' + Designer.MiddleInitial + ' ' + Designer.LastName AS DesignerName, Technicians.LastName + ' ' + Technicians.MiddleInitial + ' ' + Technicians.FirstName AS TechnicianName
+FROM Vacuum
+JOIN ModelDesigner ON Vacuum.SerialNumber = ModelDesigner.Identification_Number
+JOIN Designer ON Designer.DesignerID = ModelDesigner.Designer_ID
+JOIN VacuumTechnician ON VacuumTechnician.Serial_Number = Vacuum.SerialNumber
+JOIN Technicians ON Technicians.EmployeeNumber = VacuumTechnician.Employee_Number;
+/* Retrieves the serial number of each vacuum and it's repsective desginer & technician */
+
+/* This indicates the end of the section for retrieving all tables based off the questions */
+
+
+/* This indicates the end of the project */
